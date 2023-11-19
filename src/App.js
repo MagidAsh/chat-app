3a27095670d54d6ee25bae9b61d8fcb44ce9c00a
Majid Ash <Magid.Ash.Si@gmail.com>
2023-11-19 09:18:44 -0800

    create a react directory & install axios & install firebase@8.7.1 & install react-chat-engine@1.11.28 & install react-router-dom@6.18.0

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/"  element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
