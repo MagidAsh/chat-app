385a04b676f611f9b524cbbae58955a701354940
Majid Ash <Magid.Ash.Si@gmail.com>
2023-11-20 03:43:25 -0800

    1 - dynamization logout div  in the form of creating a function in the Chats Component and passing it as a props to Component Navbar and creating onClick event on logout div  and using of props (logoutHandler) in the onClick.
    2 - import the ChatEngine from react-chat-engine and adding the ChatEngine Component to the Chats Component and sign-up or sign-in in the site (chatengine.io) and creating a new project and get the projectID value.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
 
 // Components
 import Navbar from './Navbar';
 
 // Styles
-import styles from './Chats.module.css';
+import styles from "./Chats.module.css"
 
 const Chats = () => {
-    return (
+
+    const history = useHistory();
+
+    const logoutHandler = async () => {
+        await auth.signOut();
+        history.push("/")
+    }
+
+    return (          
+        // 100vh, which is the height value of the ChatEngine Component, is reduced from 50px, which is the height value of the Navbar Component container class, with the Calc function.
         <div className={styles.container}>
-            <Navbar />
+            <Navbar logoutHandler={logoutHandler} />
+            <ChatEngine 
+                height = "calc(100vh - 50px)"
+                projectID = "4fac6bc1-e9b0-40e8-b55a-a65897f2958b"
+                userName = "."
+                userSecret = "."
+            />
+            
         </div>
     );
 };
--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,4 +1,4 @@
-import React , {useState , useEffect, Children} from 'react';
+import React , {useState , useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { auth } from '../firebase';
 
